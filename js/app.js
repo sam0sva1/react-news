da03712b71b4de8bf0a2cf5ed64f715c61c63c86
sam0sva1 <konstantus@live.ru>
2017-02-06 13:33:54 +0300

    RN insert News component in App component

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
+var News = React.createClass({
+	render: function() {
+		return (
+			<div className="news">
+				К сожалению, новостей нет.
+			</div>
+		);
+	}
+});
+
 var App = React.createClass({
 	render: function() {
 		return (
 			<div className="app">
 				Всем привет, я компонент App!
+				<News />
 			</div>
 		)
 	}
